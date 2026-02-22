d6ab0504215fb6c319f82e9a3ee6725f69ac37d1
Cash <cashhilstad@gmail.com>
2026-02-21 20:19:13 -0800

    chore (messages, cryptography): integration
    Adjusted messages library to meet our needs, removed length functionality. Adapted to integrate with cryptography library. Updated cryptography library to handle and output strings rather than bytes.

--- lib/src/lib/cryptography/key.py
+++ lib/src/lib/cryptography/key.py
@@ -1,29 +1,49 @@
 """
-Author: Orion Hess
+Authors: Orion Hess, Cash Hilstad
 Created: 2026-02-21
 Edited: 2026-02-21
 Purpose: Manage encryption keys
 """
 from cryptography.hazmat.primitives.hpke import Suite, KEM, KDF, AEAD
 from cryptography.hazmat.primitives.asymmetric import x25519
+import base64
 
 suite = Suite(KEM.X25519, KDF.HKDF_SHA256, AEAD.AES_128_GCM)
 
 
-def generate_keypair() -> tuple[bytes, bytes]:
+def generate_keypair() -> tuple[str, str]:
     private_key = x25519.X25519PrivateKey.generate()
     public_key = private_key.public_key()
 
-    return private_key.private_bytes_raw(), public_key.public_bytes_raw()
+    private_bytes = private_key.private_bytes_raw()
+    public_bytes = public_key.public_bytes_raw()
+
+    str_keys = (
+        base64.b64encode(private_bytes).decode("utf-8"),
+        base64.b64encode(public_bytes).decode("utf-8"),
+    )
+
+    return str_keys
 
 
-def encrypt(data: bytes, public_key: bytes) -> bytes:
-    public_key_typed = x25519.X25519PublicKey.from_public_bytes(public_key)
-    cypher_text = suite.encrypt(data, public_key_typed)
+def encrypt(data: str, public_key: str) -> str:
+    data_bytes = data.encode("utf-8")
+    public_bytes = base64.b64decode(public_key)
+
+    public_key_typed = x25519.X25519PublicKey.from_public_bytes(public_bytes)
+
+    cypher_bytes = suite.encrypt(data_bytes, public_key_typed)
+    cypher_text = base64.b64encode(cypher_bytes).decode("utf-8")
+
     return cypher_text
 
+def decrypt(data: str, private_key: str) -> str:
+    data_bytes = base64.b64decode(data)
+    private_bytes = base64.b64decode(private_key)
 
-def decrypt(data: bytes, private_key: bytes) -> bytes:
-    private_key_typed = x25519.X25519PrivateKey.from_private_bytes(private_key)
-    plain_text = suite.decrypt(data, private_key_typed)
+    private_key_typed = x25519.X25519PrivateKey.from_private_bytes(private_bytes)
+
+    plain_bytes = suite.decrypt(data_bytes, private_key_typed)
+    plain_text = plain_bytes.decode("utf-8")
+    
     return plain_text
