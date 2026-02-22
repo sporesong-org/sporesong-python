6e6429da15c0d09bd88dcaf9f759840fa5193cc3
Orion Hess <orionhess@gmail.com>
2026-02-22 10:23:14 -0800

    feat(server): server impl

--- server/src/server/main.py
+++ server/src/server/main.py
@@ -1,27 +1,14 @@
 import lib.networking.client
-from server.database import Database
+from server import Server
+import asyncio
 
+s = Server()
 
-def main() -> None:
-    db = Database() 
-    db.get_connection()
-    db.drop_relations()
-    db.initialize_relations()
-    id = "I'm an ID String!"
-    name = "I'm a name!"
-    db.put_client(id, name)
-    #print("Get single client")
-    #print(db.get_client(id))
-    #print()
-    #print("Get all clients")
-    #print(db.get_clients())
+async def starting() -> None:
+    conenction_task = asyncio.create_task(s.client.connect("ws://pacific:4321"))
+    boot_task = asyncio.create_task(s.boot())
+    await conenction_task
+    
 
-    tags = {
-        "key1": "value1",
-        "key2": "value2",
-        "key3": "value3",
-        "key4": "value4",
-    }
-    sn = db.put_message("Message content", id, tags)
-    print(db.get_message(sn))
-    print(db.get_messages_sequence(0, 3))
+if __name__=="__main__":
+    asyncio.run (starting())
