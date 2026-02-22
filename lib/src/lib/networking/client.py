04d30ff4e0ee79af113fcef1d72f01bce4e2f454
Orion Hess <orionhess@gmail.com>
2026-02-21 16:19:01 -0800

    feat(networking): create initial framework

---
+++ lib/src/lib/networking/client.py
@@ -0,0 +1,13 @@
+"""
+Author: Orion Hess
+Created: 2026-02-21
+Edited: 2026-02-21
+Purpose: Functions for interacting with a connection as a client
+"""
+import socket
+
+def send_message(message: str, destination: str, proxy_address: str) -> bool:
+    pass
+
+def receive_message(length: int) -> str:
+    pass
