4a2e550f14afb90a517641b9dfbe8c14876d0449
Orion Hess <orionhess@gmail.com>
2026-02-22 08:53:22 -0800

    feat(server): implement database interfacing

--- server/src/server/database.py
+++ server/src/server/database.py
@@ -1,16 +1,30 @@
 import psycopg2
 
 from server.config import Config
+from typing import Tuple, List
 
 class Database:
     def __init__(self):
-        self.__connection = None
+        self.__connection: psycopg2.Connection = None
 
-    #def __del__(self):
-    #    if self.__connection is not None:
-    #        self.__connection.close()
+    def __del__(self):
+        if self.__connection is not None:
+            self.__connection.close()
+
+    def connection_is_alive(self) -> bool:
+        """
+        Return status of connection, printing and error if it is None
+        """
+        if self.__connection is None:
+            print("ERROR: Connection not initialized")
+            return False
+        else:
+            return True
 
     def get_connection(self) -> None:
+        """
+        Get a connection to the database
+        """
         print("STATUS: Getting database connection...")
         conn = None
         try:
@@ -20,25 +34,28 @@ class Database:
                 password = Config.DB_PASSWORD,
                 host = Config.DB_HOST,
             )
+            print("STATUS: Got database connection")
         except psycopg2.DatabaseError:
             print("ERROR: Failed to connect to database")
         
         self.__connection = conn
     
     def initialize_relations(self) -> None:
+        """
+        Initialize relations in the database - Requires empty database
+        """
         print("STATUS: Initializing database relations...")
-        if self.__connection is None:
-            print("ERROR: Connection not initialized")
+        if not self.connection_is_alive():
             return
         
         try:
             cursor = self.__connection.cursor()
             cursor.execute("""
-            CREATE TABLE user (
+            CREATE TABLE client(
             id          CHAR(32)    PRIMARY KEY,
             name        CHAR(30)    NOT NULL,
-            join_date   DATE        NOT NULL,
-            secret      CHAR(30)    NOT NULL,
+            join_date   TIMESTAMP   NOT NULL,
+            secret      CHAR(30)
             );
 
             CREATE TABLE message (
@@ -46,30 +63,203 @@ class Database:
             time            TIMESTAMP   NOT NULL,
             content         VARCHAR     NOT NULL,
             author          CHAR(32)    NOT NULL,
-            FOREIGN KEY (author) REFERENCES user (id),
+            FOREIGN KEY (author) REFERENCES client (id)
             );
 
             CREATE TABLE tag (
             message BIGSERIAL,
             key     VARCHAR,
             value   VARCHAR,
-            PRIMARY KEY (message, key, value),
+            PRIMARY KEY (message, key, value)
             ); 
             """)
+            print("STATUS: Relations initialized")
+            cursor.close()
+            self.__connection.commit()
+        # TODO: Proper exception handling
         except Exception as e:
             print(f"ERROR: {e}")
     
     def drop_relations(self) -> None:
-        print("STATUS: Dropping relations")
+        """
+        Clear relations in the database
+        """
+        print("STATUS: Dropping relations...")
         if self.__connection is None:
             print("ERROR: Connection not initialized")
+            return
+
+        try:
+            cursor = self.__connection.cursor()
+            cursor.execute("""
+            DROP TABLE IF EXISTS tag;
+            DROP TABLE IF EXISTS message;
+            DROP TABLE IF EXISTS client;
+            """)
+            print("STATUS: Relations dropped")
+            cursor.close()
+            self.__connection.commit()
+        # TODO: Proper exception handling
+        except Exception as e:
+            print(f"ERROR: {e}")
+    
+    def put_client(self, id: str, name: str, secret: str = None):
+        """
+        Put a new client in the database
+        """
+        print(f"STATUS: Storing user '{name}':('{id}')")
+        if self.__connection is None:
+            print("ERROR: Connection not initialized")
+            return
+
+        try:
+            cursor = self.__connection.cursor()
+            cursor.execute(
+                "INSERT INTO client VALUES (%s, %s, CURRENT_TIMESTAMP, %s);",
+                (id, name, secret, )
+            )
+            cursor.close()
+            self.__connection.commit()
+        # TODO: Proper exception handling
+        except Exception as e:
+            print(f"ERROR: {e}")
+        
+    def get_client(self, id: str) -> Tuple[str, str, str, str] | None:
+        print(f"STATUS: Getting user with id: '{id}'")
+        if not self.connection_is_alive:
+            return
         
         try:
             cursor = self.__connection.cursor()
             cursor.execute("""
-            DROP TABLE tag IF EXISTS;
-            DROP TABLE message IF EXISTS;
-            DROP TABLE user IF EXISTS;
-            """)
+            SELECT id, name, join_date, secret
+            FROM client
+            WHERE id = %s;
+            """, (id, ))
+            client = cursor.fetchone()
+            cursor.close()
+            self.__connection.commit()
+            return client
+        # TODO: Proper exception handling
         except Exception as e:
             print(f"ERROR: {e}")
+
+        return None
+    
+    def get_clients(self) -> List[Tuple[str, str, str, str]] | None:
+        print("STATUS: Getting users")
+        if not self.connection_is_alive:
+            return
+
+        try:
+            cursor = self.__connection.cursor()
+            cursor.execute("SELECT * FROM client")
+            clients = cursor.fetchall()
+            print(f"STATUS: Fetched {len(clients)} clients")
+            cursor.close()
+            self.__connection.commit()
+            return clients
+        # TODO: Proper exception handling
+        except Exception as e:
+            print(f"ERROR: {e}")
+
+    def put_message(self, content: str, author: str, tags: dict[str, str]) -> int | None:
+        print("STATUS: Putting message")
+        if not self.connection_is_alive:
+            return None
+        try:
+            cursor = self.__connection.cursor()
+            cursor.execute(
+                "INSERT INTO message (time, content, author) VALUES (LOCALTIMESTAMP, %s, %s) RETURNING sequence_number;",
+                (content, author, )
+            )
+            sequence_number = cursor.fetchone()[0]
+
+            tag_rows = [
+                (sequence_number, key, value) for key, value in tags.items()
+            ]
+            cursor.executemany(
+                "INSERT INTO tag VALUES (%s, %s, %s);", 
+                tag_rows
+            )
+
+            cursor.close()
+            self.__connection.commit()
+            return sequence_number
+
+        # TODO Proper error handling
+        except Exception as e:
+            print(f"ERROR: {e}")
+        return None
+
+    def get_message(self, sequence_number: int) -> Tuple[int, int, str, str, dict[str,str]]| None:
+        print(f"STATUS: Getting message with sequence_number: {sequence_number}")
+        if not self.connection_is_alive:
+            return None
+        try:
+            cursor = self.__connection.cursor()
+            cursor.execute("SELECT * FROM message WHERE sequence_number = %s;", (sequence_number, ))
+            message = cursor.fetchone()
+            if message is None: return None
+            print(message)
+            cursor.execute("SELECT key, value FROM tag WHERE message = %s", (message[0], ))
+            tags = cursor.fetchall()
+            cursor.close()
+            return (*message, dict(tags))
+        # TODO Proper error handling
+        except Exception as e:
+            print(f"ERROR: {e}")
+        return None
+
+    def get_messages_sequence(self, sequence_number_min: int, sequence_number_max: int) -> List[Tuple[int, int, str, str, dict[str, str]]] | None:
+        print(f"STATUS: Getting messages: [{sequence_number_min},{sequence_number_max}]")
+        if not self.connection_is_alive:
+            return
+        try:
+            out_messages = []
+
+            cursor = self.__connection.cursor()
+            cursor.execute(
+                "SELECT * FROM message WHERE sequence_number >= %s AND sequence_number <= %s;", 
+                (sequence_number_min, sequence_number_max, )
+            )
+            messages = cursor.fetchall()
+            if messages is None: return None
+            for message in messages:
+                print(f"The message, {message}")
+                cursor.execute("SELECT key, value FROM tag WHERE message = %s", (message[0], ))
+                tags = cursor.fetchall()
+                out_messages.append((*message, dict(tags)))
+            
+            cursor.close()
+            self.__connection.commit()
+            return out_messages
+
+        # TODO Proper error handling
+        except Exception as e:
+            print(f"ERROR: {e}")
+        return None
+
+    def get_messages_time(self, time_min: int, time_max: int) -> List[Tuple[int, int, str, str, List[str, str]]] | None:
+        print(f"STATUS: Getting messages: [{time_min},{time_max}]")
+        if not self.connection_is_alive:
+            return
+        try:
+            out_messages = []
+
+            cursor = self.__connection.cursor()
+            cursor.execute("SELECT * FROM message WHERE time >= %(time_min)s AND time <= %(time_max)s;", time_min, time_max)
+            messages = cursor.fetchone()
+            for message in messages:
+                cursor.execute("SELECT key, value FROM tag WHERE messsage = %s", message[0])
+                tags = cursor.fetch_all()
+                out_messages.append(tuple(set(message + tags)))
+            
+            cursor.close()
+            self.__connection.commit()
+            return out_messages
+
+        # TODO Proper error handling
+        except Exception as e:
+            print(f"ERROR: {e}")
+        return None
--- server/src/server/main.py
+++ server/src/server/main.py
@@ -1,8 +1,27 @@
 import lib.networking.client
 from server.database import Database
 
-db = Database() 
-db.get_connection()
-db.drop_relations()
-db.initialize_relations()
 
+def main() -> None:
+    db = Database() 
+    db.get_connection()
+    db.drop_relations()
+    db.initialize_relations()
+    id = "I'm an ID String!"
+    name = "I'm a name!"
+    db.put_client(id, name)
+    #print("Get single client")
+    #print(db.get_client(id))
+    #print()
+    #print("Get all clients")
+    #print(db.get_clients())
+
+    tags = {
+        "key1": "value1",
+        "key2": "value2",
+        "key3": "value3",
+        "key4": "value4",
+    }
+    sn = db.put_message("Message content", id, tags)
+    print(db.get_message(sn))
+    print(db.get_messages_sequence(0, 3))
